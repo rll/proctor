c22ebca6f26bfee1df315954c456cc33b858f947
Sergey Karayev <sergeykarayev@gmail.com>
2011-09-15 10:42:12 -0700

    refactor evaluation code

---
+++ plotting/evaluation.py
@@ -0,0 +1,107 @@
+import re
+from pylab import *
+
+class Bunch:
+  def __init__(self, **kwds):
+    self.__dict__.update(kwds)
+  def dict(self):
+    return self.__dict__
+
+class Evaluation:
+  def __init__(self, eval_set, feature):
+    log_filename = eval_set.log_location_template%feature 
+    self.feature = feature
+
+    with open(log_filename) as log:
+      lines = log.readlines()
+
+    self.trials = []
+    i = 0
+    while i < len(lines):
+      l = lines[i]
+
+      # Model test output
+      if l.find('[test') > -1:
+        trial = Bunch() 
+        trial.trial_num = int(re.search('test (\d+)', l).group(1))
+        trial.model_ind = int(re.search('scanned model (\d+)', lines[i+1]).group(1))
+        trial.model_at_rank = []
+        try:
+          for j in range(4):
+            # get the first number on the line
+            trial.model_at_rank.append(int(re.search('^(\d+)', lines[i+3+j]).group(1)))
+          trial.detector_guess = int(re.search('detector guessed (\d+)', lines[i+3+4]).group(1))
+          self.trials.append(trial)
+        except:
+          None
+          # it's okay, detector just failed to find matches
+        self.num_trials = len(self.trials)
+      
+      # Overview output
+      if l.find('[overview]') > -1:
+        m = re.search('^(\d+)', lines[i+1]) # first number
+        self.num_correct = int(m.group(1))
+
+      # Precision-Recall output
+      if l.find('[precision-recall]') > -1:
+        self.precision = []
+        self.recall = []
+        j = 1
+        while True:
+          if lines[i+j].find('[classifier stats]') > -1:
+            break
+          numbers = lines[i+j].split()
+          self.precision.append(float(numbers[0]))
+          self.recall.append(float(numbers[1]))
+          j += 1
+        self.ap = VOCap(self.recall,self.precision)
+
+      # Some more statistics about the classifier
+      if l.find('[classifier stats]') > -1:
+        # get the last numbers in the next two lines
+        self.avg_rank = float(lines[i+1].split()[-1])
+        self.auh = float(lines[i+2].split()[-1])
+        self.auh /= eval_set.num_models*self.num_trials
+
+      # Confusion matrix
+      if l.find('[confusion matrix]') > -1:
+        # get the num_models lines following this one
+        data = lines[i+1:i+1+eval_set.num_models]
+        data = [line.strip() for line in data]
+        self.confusion_matrix = matrix(';'.join(data))
+
+      # Timing info
+      if l.find('[timing]') > -1:
+        print("TIMING HERE")
+        # TODO: do timing info 
+      i += 1
+
+  def rank_histogram_data(self):
+    # Go through and count the number of times the right model was rank-1,
+    # rank-2, etc.
+    # construct matrix of all the model_at_ranks
+    all_model_at_rank = zeros((self.num_trials,4))
+    correct_model = zeros((self.num_trials,1))
+    for i,trial in enumerate(self.trials):
+      all_model_at_rank[i,:] = trial.model_at_rank
+      correct_model[i] = trial.model_ind
+    # compare to the correct model
+    correct_model_at_rank = all_model_at_rank == tile(correct_model,(1,4))
+    # add a last column, which gets a 1 if none of the other columns have a 1
+    catchall_rank_col = array(sum(correct_model_at_rank,1)>0)
+    catchall_rank_col = transpose([catchall_rank_col]) # SERIOUSLY?
+    correct_model_at_rank = concatenate((correct_model_at_rank,catchall_rank_col),1)
+    counts = sum(correct_model_at_rank,0)
+    return counts
+
+def VOCap(rec,prec):
+  mprec = hstack((0,prec,0))
+  mrec = hstack((0,rec,1))
+  # make sure prec is monotonically decreasing
+  for i in range(len(mprec)-1,0,-1):
+    mprec[i-1]=max(mprec[i-1],mprec[i])
+  # find piecewise area under the curve
+  i = add(nonzero(mrec[1:] != mrec[0:-1]),1)
+  ap=sum((mrec[i]-mrec[subtract(i,1)])*mprec[i])
+  return ap
+
--- plotting/main.py
+++ plotting/main.py
@@ -9,216 +9,127 @@ from pprint import pprint
 import copy
 from pylab import *
 
-class Bunch:
-  def __init__(self, **kwds):
-    self.__dict__.update(kwds)
-  def dict(self):
-    return self.__dict__
+from evaluation import Evaluation
 
-def main():
-  """Goes through different features, outputting their evaluations, as well as
-  a combined PR curve evaluation."""
+class EvalSet:
+  def plot_pr(self,features=None):
+    """Takes a list of features and plots their PR curves on the same plot."""
+    if not features:
+      features = self.features
+    clf()
+    linestyles = ['-', '--', ':']
+    for i,f in enumerate(features):
+      e = self.evals[f]
+      label = "%s: %.3f"%(e.feature,e.ap)
+      lstyle = linestyles[mod(i,len(linestyles))]
+      plot(e.recall,e.precision,lstyle,label=label)
+    legend()
+    xlabel('Recall')
+    ylabel('Precision')
+    grid(True)
 
-  # First, load the common model names
-  model_name_location = "../writeups/model_names.txt"
-  with open(model_name_location) as f:
-    lines = f.readlines()
-  model_names = [line.strip() for line in lines]
+    filename = self.plot_location+"%s_pr.png"%('-'.join([f for f in features]))
+    savefig(filename)
 
-  features = ['PFH','FPFH','SHOT','SPIN_IMAGE']
-  evaluations = []
-  for feature in features:
-    e = Bunch()
-    e.dataset="PSB"
-    e.feature=feature
-    e.data = parse_log(e)
-    e.model_names = model_names
-    print("Feature: %s"%e.feature)
+  def print_comparison_table(self, features=None):
+    # TODO
+    return
+
+  def print_info(self, feature):
+    e = self.evals[feature]
+    print("Feature: %s"%feature)
     print("  Num correct: %s"%e.num_correct)
     print("  AP: %s"%e.ap)
     print("  Average rank: %s"%e.avg_rank)
     print("  AUH: %s"%e.auh)
 
-    e.plot_location = "../writeups/figures/"
-    if not os.path.exists(e.plot_location):
-      os.makedirs(e.plot_location)
-    
-    evaluations.append(e)
+  def plot_rank_histogram(self, feature):
+    """Takes an evaluation and outputs its rank histogram to file."""
+    e = self.evals[feature]
 
-    plot_pr([e])
-    plot_confusion_matrix(e)
-    plot_rank_histogram(e)
-  plot_pr(evaluations)
+    clf()
+    counts = e.rank_histogram_data() 
+    counts_r = 1.*counts/sum(counts)
+    ind = arange(5)
+    width = 0.9
+    bar(ind,cumsum(counts_r),width=.9,color='black')
+    for i,count in enumerate(cumsum(counts_r)):
+      text(i+width/2.-.1, count*1.-.04, "%.3f"%count, color='white')
+    ranks = ['1','2','3','4','5+']
+    xticks(ind+width/2., ranks) 
+    xlabel('Number of top results within which the correct model is fetched')
+    #ylabel('Proportion')
+    title('Area under the rank histogram is %.3f.'%e.auh)
 
-def parse_log(e):
-  e.log_location = "../writeups/%s.out"%e.feature.lower()
-  with open(e.log_location) as log:
-    lines = log.readlines()
+    filename = self.plot_location+"%s_rankhist.png"%e.feature
+    savefig(filename)
 
-  e.trials = []
-  i = 0
-  while i < len(lines):
-    l = lines[i]
+  def plot_confusion_matrix(self,feature):
+    """Takes an evaluation and outputs its confusion matrix to file."""
+    # code from http://stackoverflow.com/questions/2897826/confusion-matrix-with-number-of-classified-misclassified-instances-on-it-python
+    e = self.evals[feature]
+    conf_arr = e.confusion_matrix.tolist()
 
-    # Get number of models
-    if l.find('[training]') > -1:
-      # get the integer in the line above this one
-      highest_model_ind = int(re.search('finished model (\d+)', lines[i-1]).group(1))
-      e.num_models = highest_model_ind+1
+    norm_conf = []
+    for i in conf_arr:
+      a = 0
+      tmp_arr = []
+      a = sum(i,0)
+      for j in i:
+        tmp_arr.append(float(j)/float(a))
+      norm_conf.append(tmp_arr)
 
-    # Model test output
-    if l.find('[test') > -1:
-      trial = Bunch() 
-      trial.trial_num = int(re.search('test (\d+)', l).group(1))
-      trial.model_ind = int(re.search('scanned model (\d+)', lines[i+1]).group(1))
-      trial.model_at_rank = []
-      try:
-        for j in range(4):
-          # get the first number on the line
-          trial.model_at_rank.append(int(re.search('^(\d+)', lines[i+3+j]).group(1)))
-        trial.detector_guess = int(re.search('detector guessed (\d+)', lines[i+3+4]).group(1))
-        e.trials.append(trial)
-      except:
-        None
-        # it's okay, detector just failed to find matches
-      e.num_trials = len(e.trials)
-    
-    # Overview output
-    if l.find('[overview]') > -1:
-      m = re.search('^(\d+)', lines[i+1]) # first number
-      e.num_correct = int(m.group(1))
+    clf()
+    fig = figure()
+    ax = fig.add_subplot(111)
+    res = ax.imshow(array(norm_conf), cmap=cm.gray_r, interpolation='nearest')
+    for i, cas in enumerate(norm_conf):
+      for j, c in enumerate(cas):
+        if c>0:
+          # TODO: adjust the location of the text
+          if c <= 0.5:
+            text(j-.2, i+.2, "%.2f"%c, color='black', fontsize=12)
+          else:
+            text(j-.2, i+.2, "%.2f"%c, color='white', fontsize=12)
+    cb = fig.colorbar(res)
+    xticks(arange(0, self.num_models), self.model_names, rotation=30, size='small')
+    yticks(arange(0, self.num_models), self.model_names, size='small')
 
-    # Precision-Recall output
-    if l.find('[precision-recall]') > -1:
-      e.precision = []
-      e.recall = []
-      j = 1
-      while True:
-        if lines[i+j].find('[classifier stats]') > -1:
-          break
-        numbers = lines[i+j].split()
-        e.precision.append(float(numbers[0]))
-        e.recall.append(float(numbers[1]))
-        j += 1
-      e.ap = VOCap(e.recall,e.precision)
+    filename = self.plot_location+"%s_confmat.png"%e.feature
+    savefig(filename)
 
-    # Some more statistics about the classifier
-    if l.find('[classifier stats]') > -1:
-      # get the last numbers in the next two lines
-      e.avg_rank = float(lines[i+1].split()[-1])
-      e.auh = float(lines[i+2].split()[-1])
-      e.auh /= e.num_models*e.num_trials
+def main():
+  """Goes through different features, outputting their evaluations, as well as
+  a combined PR curve evaluation."""
+  eval_set = EvalSet()
 
-    # Confusion matrix
-    if l.find('[confusion matrix]') > -1:
-      # get the num_models lines following this one
-      data = lines[i+1:i+1+e.num_models]
-      data = [line.strip() for line in data]
-      e.confusion_matrix = matrix(';'.join(data))
+  # First, load the common model names
+  model_name_location = "../writeups/model_names.txt"
+  with open(model_name_location) as f:
+    lines = f.readlines()
+  eval_set.model_names = [line.strip() for line in lines]
+  eval_set.num_models = len(eval_set.model_names)
+  eval_set.dataset = "PSB"
+  eval_set.log_location_template = "../writeups/%s.out"
+  eval_set.plot_location = "../writeups/figures/"
+  if not os.path.exists(eval_set.plot_location):
+    os.makedirs(eval_set.plot_location)
 
-    # Timing info
-    if l.find('[timing]') > -1:
-      print("TIMING HERE")
-      # TODO: do timing info 
-    i += 1
+  # Parse the output log data for all the evaluations
+  eval_set.features = ['PFH','FPFH','SHOT','SPIN_IMAGE']
+  eval_set.evals = {}
+  for feature in eval_set.features:
+    eval_set.evals[feature] = Evaluation(eval_set, feature)
+    # print the info
+    eval_set.print_info(feature)
+    # output plots
+    eval_set.plot_confusion_matrix(feature)
+    eval_set.plot_rank_histogram(feature)
+    eval_set.plot_pr([feature])
 
-def rank_histogram_data(e):
-  # Go through and count the number of times the right model was rank-1,
-  # rank-2, etc.
-  # construct matrix of all the model_at_ranks
-  all_model_at_rank = zeros((e.num_trials,4))
-  correct_model = zeros((e.num_trials,1))
-  for i,trial in enumerate(e.trials):
-    all_model_at_rank[i,:] = trial.model_at_rank
-    correct_model[i] = trial.model_ind
-  # compare to the correct model
-  correct_model_at_rank = all_model_at_rank == tile(correct_model,(1,4))
-  # add a last column, which gets a 1 if none of the other columns have a 1
-  catchall_rank_col = array(sum(correct_model_at_rank,1)>0)
-  catchall_rank_col = transpose([catchall_rank_col]) # SERIOUSLY?
-  correct_model_at_rank = concatenate((correct_model_at_rank,catchall_rank_col),1)
-  counts = sum(correct_model_at_rank,0)
-  return counts
-
-def plot_rank_histogram(e):
-  """Takes an evaluation and outputs its rank histogram to file."""
-  clf()
-  counts = rank_histogram_data(e) 
-  counts_r = 1.*counts/sum(counts)
-  ind = arange(5)
-  width = 0.9
-  bar(ind,cumsum(counts_r),width=.9,color='black')
-  for i,count in enumerate(cumsum(counts_r)):
-    text(i+width/2.-.1, count*1.-.04, "%.3f"%count, color='white')
-  ranks = ['1','2','3','4','5+']
-  xticks(ind+width/2., ranks) 
-  xlabel('Number of top results within which the correct model is fetched')
-  #ylabel('Proportion')
-  title('Area under the rank histogram is %.3f.'%e.auh)
-
-  filename = e.plot_location+"%s_rankhist.png"%e.feature
-  savefig(filename)
-
-def plot_confusion_matrix(e):
-  """Takes an evaluation and outputs its confusion matrix to file."""
-  # code from http://stackoverflow.com/questions/2897826/confusion-matrix-with-number-of-classified-misclassified-instances-on-it-python
-  conf_arr = e.confusion_matrix.tolist()
-
-  norm_conf = []
-  for i in conf_arr:
-    a = 0
-    tmp_arr = []
-    a = sum(i,0)
-    for j in i:
-      tmp_arr.append(float(j)/float(a))
-    norm_conf.append(tmp_arr)
-
-  clf()
-  fig = figure()
-  ax = fig.add_subplot(111)
-  res = ax.imshow(array(norm_conf), cmap=cm.gray_r, interpolation='nearest')
-  for i, cas in enumerate(norm_conf):
-    for j, c in enumerate(cas):
-      if c>0:
-        # TODO: adjust the location of the text
-        if c <= 0.5:
-          text(j-.2, i+.2, "%.2f"%c, color='black', fontsize=12)
-        else:
-          text(j-.2, i+.2, "%.2f"%c, color='white', fontsize=12)
-  cb = fig.colorbar(res)
-  xticks(arange(0,e.num_models), e.model_names, rotation=30, size='small')
-  yticks(arange(0,e.num_models), e.model_names, size='small')
-
-  filename = e.plot_location+"%s_confmat.png"%e.feature
-  savefig(filename)
-
-def plot_pr(evaluations):
-  """Takes a list of evaluations and plots their PR curves."""
-  clf()
-  linestyles = ['-', '--', ':']
-  for i,e in enumerate(evaluations):
-    label = "%s: %.3f"%(e.feature,e.ap)
-    lstyle = linestyles[mod(i,len(linestyles))]
-    plot(e.recall,e.precision,lstyle,label=label)
-  legend()
-  xlabel('Recall')
-  ylabel('Precision')
-  grid(True)
-
-  e = evaluations[0]
-  filename = e.plot_location+"%s_pr.png"%('-'.join([e.feature for e in evaluations]))
-  savefig(filename)
-
-def VOCap(rec,prec):
-  mprec = hstack((0,prec,0))
-  mrec = hstack((0,rec,1))
-  # make sure prec is monotonically decreasing
-  for i in range(len(mprec)-1,0,-1):
-    mprec[i-1]=max(mprec[i-1],mprec[i])
-  # find piecewise area under the curve
-  i = add(nonzero(mrec[1:] != mrec[0:-1]),1)
-  ap=sum((mrec[i]-mrec[subtract(i,1)])*mprec[i])
-  return ap
+  # Print feature comparison table and output PR plot
+  eval_set.print_comparison_table()
+  eval_set.plot_pr()
 
 if __name__ == '__main__':
   main()
